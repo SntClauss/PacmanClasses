47f3d811f509ddf5c05fc1a873572607f4e0c3f2
Jimmy Toledo <jimmy.toledo.a@gmail.com>
2018-06-07 17:53:37 -0400

    Added character and maze classes

---
+++ classes.py
@@ -0,0 +1,70 @@
+from enum import Enum
+
+class GhostColor(Enum):
+    pink = 0
+    blue = 1
+    orange = 2 
+    red = 3
+class Direction(Enum):
+    top = 0
+    bottom = 1
+    left = 2 
+    right = 3
+class Status(Enum):
+    alive = 0
+    dead = 1
+    frightened = 2 
+    powered = 3
+
+class Character:
+    direction = Direction.top
+    speed = 0
+    status = Status.alive
+    x = 0
+    y = 0
+    def die():
+        return
+    def spawn(x,y):
+        return
+    def move(direction):
+        return
+    def teleport(x,y):
+        return
+class Ghost(Character):
+    color = GhostColor.red
+    status = Status.alive
+    scatterTimer = 0
+    frightenedTimer = 0
+    def updateScore():
+        return
+    def leaveCage():
+        return
+    def getBackToCage():
+        return
+    def setFrightened(seconds):
+        return
+    def scatter(seconds):
+        return
+    def decideDirection(pacX,pacY):
+        return
+class Pacman(Character):
+    killStreak = 0
+    lastControl = Direction.top
+    def control(direction):
+        return
+    def collide():
+        return
+class Maze():
+    score = 0
+    lives = 3
+    tiles = None
+    def pillsLeft():
+        return
+    def addToScore():
+        return
+    def killGhost():
+        return
+    def reset():
+        return
+    def win():
+        return
